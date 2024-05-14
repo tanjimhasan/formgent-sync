7fe05ef789b430c88f3beab942befe0bb9946162
MD AL AMIN <mdalaminbey@gmail.com>
2024-05-14 10:40:37 +0600

    Update admin-enqueue.php

--- enqueues/admin-enqueue.php
+++ enqueues/admin-enqueue.php
@@ -4,6 +4,6 @@ use NewForm\WpMVC\Enqueue\Enqueue;
 
 defined( 'ABSPATH' ) || exit;
 
-Enqueue::register_script('newform/components', 'build/js/components');
+Enqueue::register_script( 'newform/components', 'build/js/components' );
 
 Enqueue::script( 'newform-admin', 'build/js/admin' );
