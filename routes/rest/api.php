f1919ece01156b671db436a6d2932f9853416452
MD AL AMIN <mdalaminbey@gmail.com>
2024-05-14 10:39:10 +0600

    Update api.php

--- routes/rest/api.php
+++ routes/rest/api.php
@@ -1,6 +1,33 @@
 <?php
 
 use NewForm\App\Http\Controllers\UserController;
+use NewForm\WpMVC\Routing\Response;
 use NewForm\WpMVC\Routing\Route;
 
 Route::get( 'user', [UserController::class, 'index'], ['admin'] );
+
+Route::get(
+    'ping', function() {
+        return Response::send(
+            [
+                'message' => 'pong',
+                'data'    => [
+                    'hello' => 'world'
+                ]
+            ]
+        );
+    }
+);
+
+Route::post(
+    'ping', function() {
+        return Response::send(
+            [
+                'message' => 'pong',
+                'data'    => [
+                    'hello' => 'world'
+                ]
+            ]
+        );
+    }
+);
