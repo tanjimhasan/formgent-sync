ee32a7a19db34806b9a9117be94e53950e7f4057
MD AL AMIN <mdalaminbey@gmail.com>
2024-05-14 18:37:10 +0600

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,8 @@ namespace NewForm\App\Http\Controllers;
 abstract class Controller {
     protected function pagination( string $path, int $total_items, int $per_page = 10 ) {
         $params       = $this->get_query_params();
-        $current_page = intval( $params['page'] ) ?? 1;
-        $per_page     = $params['per_page'] ?? $per_page;
+        $current_page = isset( $params['page'] ) ? intval( $params['page'] ) : 1;
+        $per_page     = isset( $params['per_page'] ) ? intval( $params['per_page'] ) : $per_page;
         $total_pages  = ceil( $total_items / $per_page );
         $rest_url     = get_rest_url( null, $path );
 
@@ -18,13 +18,17 @@ abstract class Controller {
             ],
             'links'      => [
                 'prev' => $this->get_prev_link( $params, $rest_url ),
-                'self' =>  add_query_arg( $params, $rest_url ),
+                'self' => add_query_arg( $params, $rest_url ),
                 'next' => $this->get_next_link( $params, $rest_url, $total_pages )
             ]
         ];
     }
 
     protected function get_query_params() {
+        /**
+         * This method is accessible only via REST request and utilizes QUERY_STRING for generating pagination links. No data is stored in the database from this method, hence validation is unnecessary.
+         */
+        // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotValidated, WordPress.Security.ValidatedSanitizedInput.MissingUnslash, WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
         parse_str( $_SERVER['QUERY_STRING'], $params );
         return $params;
     }
@@ -36,13 +40,13 @@ abstract class Controller {
             return null;
         }
     
-        $args['page'] = $page  + 1;
+        $args['page'] = $page + 1;
 
         return add_query_arg( $args, $rest_url ); 
     }
 
     private function get_prev_link( array $args, string $rest_url ) {
-        if ( empty( $args['page'] ) || 1 == $args['page'] ) {
+        if ( empty( $args['page'] ) || ! is_int( $args['page'] ) || 1 == $args['page'] ) {
             return null;
         }
         
