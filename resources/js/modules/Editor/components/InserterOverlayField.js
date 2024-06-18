d1ebb45c3d27d92443b5fbcea01dd7e9fd4f4b61
tanjim hasan <tanjim.hasan.pro@gmail.com>
2024-06-18 07:16:30 +0600

    updated

--- resources/js/modules/Editor/components/InserterOverlayField.js
+++ resources/js/modules/Editor/components/InserterOverlayField.js
@@ -1,6 +1,5 @@
 export default function InserterOverlayField( props ) {
 	const { inserterOverlayActiveField } = props;
-	console.log( inserterOverlayActiveField );
 	return (
 		<div className="formgent-editor-inserter__field">
 			{ inserterOverlayActiveField.type }
