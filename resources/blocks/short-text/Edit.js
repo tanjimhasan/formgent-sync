42bcd1ad48fd97c234c9431b22078da774095678
iamibrahimriaz <ibrahimriaz.sovware@gmail.com>
2024-07-03 15:50:36 +0600

    Updated

--- resources/blocks/short-text/Edit.js
+++ resources/blocks/short-text/Edit.js
@@ -3,9 +3,9 @@ import { useBlockProps } from '@wordpress/block-editor';
 export default function Edit( { attributes, setAttributes } ) {
 	return (
 		<p { ...useBlockProps() }>
-			<code>
+			{ /* <code>
 				<pre>{ JSON.stringify( attributes, null, 2 ) }</pre>
-			</code>
+			</code> */ }
 			<div className="formgent-form-group">
 				<label>{ attributes.label }</label>
 				<input type="text" value={ attributes.value } />
