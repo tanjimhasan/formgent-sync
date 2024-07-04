aa3d096e9dab83c45ea864fe9435e004cb8466e9
iamibrahimriaz <ibrahimriaz.sovware@gmail.com>
2024-07-04 12:47:43 +0600

    Updated

--- resources/blocks/long-text/Edit.js
+++ resources/blocks/long-text/Edit.js
@@ -3,16 +3,13 @@ import { useBlockProps } from '@wordpress/block-editor';
 export default function Edit( { attributes, setAttributes } ) {
 	return (
 		<p { ...useBlockProps() }>
-			{ /* <code>
-				<pre>{ JSON.stringify( attributes, null, 2 ) }</pre>
-			</code> */ }
 			<div className="formgent-form-group">
 				<label>
 					{ attributes.label } { attributes.required ? '*' : '' }
 				</label>
 				<textarea
-					value={ attributes.value }
 					name={ attributes.name }
+					placeholder={ attributes.placeholder }
 					rows="4"
 					cols="50"
 				/>
