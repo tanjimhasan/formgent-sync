537679974c7d9c8e2a79ec6fb2850e8a4cacda86
iamibrahimriaz <ibrahimriaz.sovware@gmail.com>
2024-07-04 12:22:20 +0600

    Updated

--- resources/blocks/number/Edit.js
+++ resources/blocks/number/Edit.js
@@ -3,9 +3,6 @@ import { useBlockProps } from '@wordpress/block-editor';
 export default function Edit( { attributes, setAttributes } ) {
 	return (
 		<div { ...useBlockProps() }>
-			<code>
-				<pre>{ JSON.stringify( attributes, null, 2 ) }</pre>
-			</code>
 			<div className="formgent-form-group">
 				<label>
 					{ attributes.label } { attributes.required ? '*' : '' }
