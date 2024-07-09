077c13fe35ed5f52a1e082abca5e9c08ce038089
Syed Galib Ahmed <syedgalibahmed@gmail.com>
2024-07-09 15:17:37 +0600

    updated

--- app/Http/Controllers/Admin/AnalyticsController.php
+++ app/Http/Controllers/Admin/AnalyticsController.php
@@ -6,17 +6,22 @@ defined( 'ABSPATH' ) || exit;
 
 use FormGent\App\Http\Controllers\Controller;
 use FormGent\App\Repositories\AnalyticRepository;
+use FormGent\App\Repositories\FormRepository;
 
 use FormGent\WpMVC\Routing\Response;
 use FormGent\WpMVC\RequestValidator\Validator;
+
 use WP_REST_Request;
 use Exception;
 
 class AnalyticsController extends Controller {
     public AnalyticRepository $analytic_repository;
+
+    public FormRepository $form_repository;
     
-    public function __construct( AnalyticRepository $analytic_repository ) {
+    public function __construct( AnalyticRepository $analytic_repository, FormRepository $form_repository ) {
         $this->analytic_repository = $analytic_repository;
+        $this->form_repository     = $form_repository;
     }
 
     public function form_summary( Validator $validator, WP_REST_Request $wp_rest_request ) {
@@ -74,6 +79,16 @@ class AnalyticsController extends Controller {
         }
 
         try {
+            $form = $this->form_repository->get_by_id( absint( $wp_rest_request->get_param( 'id' ) ) );
+
+            if ( ! $form ) {
+                return Response::send(
+                    [
+                        'messages' => esc_html__( 'Form not found', 'formgent' )
+                    ], 404
+                );
+            }
+
             return Response::send(
                 [
                     'new_count' => $this->analytic_repository->update_form_view_count(
