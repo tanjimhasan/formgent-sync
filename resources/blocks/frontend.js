2ab5ebcc1d36990309078643cd96b9e620e9a9ae
mdalaminbey <mdalaminbey@gmail.com>
2024-07-30 22:02:05 +0600

    use interactivity context instead of FormData

--- resources/blocks/frontend.js
+++ resources/blocks/frontend.js
@@ -3,7 +3,7 @@
  */
 import { store, getContext, getElement } from '@wordpress/interactivity';
 
-store( 'formgent-block', {
+store( 'formgent/form', {
 	actions: {
 		toggle: () => {
 			const context = getContext();
@@ -14,54 +14,51 @@ store( 'formgent-block', {
 			const context = getContext();
 			context[ element.ref.name ] = element.ref.value;
 		},
-
 		updateNumber: () => {
 			const element = getElement();
 			const context = getContext();
 			context[ element.ref.name ] = parseInt( element.ref.value );
 		},
+		submit: async () => {
+			const context = getContext();
+			const element = getElement();
+
+			const formData = [];
+
+			for ( const name in context.data ) {
+				if ( ! Object.hasOwnProperty.call( context.data, name ) ) {
+					continue;
+				}
+				const value = context.data[ name ];
+
+				switch ( context.blocksSettings[ name ].field_type ) {
+					case 'number':
+						formData[ name ] = parseInt( value, 10 );
+						break;
+					default:
+						formData[ name ] = value;
+						break;
+				}
+			}
+
+			try {
+				const response = await wp.apiFetch( {
+					path: '/formgent/responses',
+					method: 'POST',
+					data: {
+						id: context.formId,
+						form_data: formData,
+					},
+				} );
+				const form = element.ref.closest( 'form' );
+				form.querySelector(
+					'.formgent-notices'
+				).innerHTML = `<span>${ response.message }</span>`;
+				form.reset();
+			} catch ( error ) {
+				console.error( 'Error:', error );
+			}
+		},
 	},
 	callbacks: {},
-} );
-
-document.addEventListener( 'formgent/submit', async function ( event ) {
-	const { formId, names, form } = event.detail;
-
-	const formData = new FormData( form );
-	const rowData = {};
-
-	for ( const name of Object.keys( names ) ) {
-		const inputElement = form.querySelector( `[name="${ name }"]` );
-		const inputType = inputElement ? inputElement.type : null;
-
-		switch ( inputType ) {
-			case 'number':
-				rowData[ name ] = parseInt( formData.get( name ), 10 );
-				break;
-			default:
-				rowData[ name ] = formData.get( name );
-				break;
-		}
-	}
-
-	const submissionData = {
-		id: parseInt( formId ),
-		form_data: {
-			...rowData,
-		},
-	};
-
-	try {
-		const response = await wp.apiFetch( {
-			path: '/formgent/responses',
-			method: 'POST',
-			data: submissionData,
-		} );
-
-		const noticeContainer = form.querySelector( '.formgent-notices' );
-		noticeContainer.innerHTML = `<span>${ response.message }</span>`;
-		form.reset();
-	} catch ( error ) {
-		console.error( 'Error:', error );
-	}
 } );
--- resources/views/form.php
+++ resources/views/form.php
@@ -3,26 +3,32 @@
 wp_enqueue_script( 'wp-api-fetch' );
 wp_enqueue_script( 'formgent/just-validate' );
 
-$data  = formgent_get_form_field_settings( parse_blocks( $form->post_content ) );
-$names = array_map(
+$data      = formgent_get_form_field_settings( parse_blocks( $form->post_content ) );
+$form_data = array_map(
     function( $item ) {
         return isset( $item['value'] ) ? $item['value'] : '';
     }, $data
 );
 
-$names_json = wp_json_encode( $names );
-$unique_id  = str_replace( '-', '_', wp_unique_id( 'formgent-store' ) );
+$unique_id = str_replace( '-', '_', wp_unique_id( 'formgent-store' ) );
+
+$context = [
+    'formId'         => $form->ID,
+    'blocksSettings' => $data,
+    'data'           => $form_data,
+];
 
 ?>
 <h1>FormGent Form</h1>
-<div class="formgent-form-submisison">
+<div class="formgent-form">
     <form
         id="formgent-<?php formgent_render( $unique_id ) ?>"
         <?php formgent_render( get_block_wrapper_attributes() ); ?>
-        data-wp-interactive="formgent-block"
-        data-wp-context='<?php formgent_render( $names_json ); ?>'
+        data-wp-interactive="formgent/form"
+        data-wp-context='<?php formgent_render( wp_json_encode( $context ) ); ?>'
     >
         <div class="formgent-notices"></div>
+        <div class="formgent-submit" data-wp-on--click="actions.submit"></div>
         <?php formgent_render( $fields )?>
         <button type="submit" class="formgent-btn formgent-primary formgent-btn-md">Submit</button>
     </form>
@@ -70,13 +76,7 @@ $unique_id  = str_replace( '-', '_', wp_unique_id( 'formgent-store' ) );
 
             validation.addField( selector,rules ).onSuccess( (event) => {
                 event.preventDefault();
-                document.dispatchEvent(new CustomEvent('formgent/submit', {
-                    detail: {
-                        formId: <?php formgent_render( $form->ID );?>,
-                        names: <?php formgent_render( $names_json ); ?>,
-                        form: event.target 
-                    }
-                }));  
+                event.target.querySelector(".formgent-submit").click()
             });
         });
     })();
