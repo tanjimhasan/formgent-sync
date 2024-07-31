758a28724a3d06b987010593008bd13141aaadbf
mdalaminbey <mdalaminbey@gmail.com>
2024-07-31 10:39:34 +0600

    Update frontend.js

--- resources/blocks/frontend.js
+++ resources/blocks/frontend.js
@@ -5,19 +5,15 @@ import { store, getContext, getElement } from '@wordpress/interactivity';
 
 store( 'formgent/form', {
 	actions: {
-		toggle: () => {
-			const context = getContext();
-			context.isOpen = ! context.isOpen;
-		},
 		updateInput: () => {
 			const element = getElement();
 			const context = getContext();
-			context[ element.ref.name ] = element.ref.value;
+			context.data[ element.ref.name ] = element.ref.value;
 		},
 		updateNumber: () => {
 			const element = getElement();
 			const context = getContext();
-			context[ element.ref.name ] = parseInt( element.ref.value );
+			context.data[ element.ref.name ] = parseInt( element.ref.value );
 		},
 		submit: async () => {
 			const context = getContext();
