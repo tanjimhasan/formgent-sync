a6894eed7999982fb540f134d1d2c054270906e7
mdalaminbey <mdalaminbey@gmail.com>
2024-08-02 02:30:35 +0600

    improved form just-validate

--- resources/blocks/frontend.js
+++ resources/blocks/frontend.js
@@ -2,8 +2,9 @@
  * WordPress dependencies
  */
 import { store, getContext, getElement } from '@wordpress/interactivity';
+import JustValidate from 'just-validate';
 
-store( 'formgent/form', {
+const { callbacks } = store( 'formgent/form', {
 	actions: {
 		updateInput: () => {
 			const element = getElement();
@@ -15,10 +16,59 @@ store( 'formgent/form', {
 			const context = getContext();
 			context.data[ element.ref.name ] = parseInt( element.ref.value );
 		},
-		submit: async () => {
+	},
+	callbacks: {
+		init: () => {
 			const context = getContext();
 			const element = getElement();
 
+			const validation = new JustValidate(
+				`#${ element.attributes.id }`
+			);
+
+			let rulesList = {
+				email: function ( rules ) {
+					rules.push( {
+						rule: 'email',
+						errorMessage: 'Email is invalid!',
+					} );
+				},
+			};
+
+			//Loop over each field & prepare validation group
+			for ( const name in context.data ) {
+				const selector = element.ref.querySelector(
+					`input[name="${ name }"]`
+				);
+				const rules = [];
+				const field = context.blocksSettings[ name ];
+
+				// General validation rules
+				if ( field.required ) {
+					rules.push( {
+						rule: 'required',
+						errorMessage: `${ field.label } is required`,
+					} );
+				}
+
+				// Type specific validation rules
+				if ( rulesList[ field.field_type ] ) {
+					rulesList[ field.field_type ]( rules );
+				}
+
+				if ( rules.length < 1 ) {
+					return;
+				}
+
+				validation.addField( selector, rules );
+			}
+
+			validation.onSuccess( ( event ) => {
+				event.preventDefault();
+				callbacks.submit( context, element );
+			} );
+		},
+		submit: async ( context, element ) => {
 			const formData = {};
 
 			for ( const name in context.data ) {
@@ -46,15 +96,17 @@ store( 'formgent/form', {
 						form_data: formData,
 					},
 				} );
+
 				const form = element.ref.closest( 'form' );
 				form.querySelector(
 					'.formgent-notices'
 				).innerHTML = `<span>${ response.message }</span>`;
+
 				form.reset();
+				context.data = {};
 			} catch ( error ) {
 				console.error( 'Error:', error );
 			}
 		},
 	},
-	callbacks: {},
 } );
