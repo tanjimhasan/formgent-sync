63ce0f72bc37ea3640ae25651efca17a6ad74531
iamibrahimriaz <ibrahimriaz.sovware@gmail.com>
2024-08-04 20:54:25 +0600

    Blocks - Re Added after Revert

--- resources/blocks/multi-choice/index.js
+++ resources/blocks/multi-choice/index.js
@@ -9,6 +9,7 @@ import { __ } from '@wordpress/i18n';
 import { registerBlock } from '../utils';
 import Edit from './Edit';
 import metadata from './block.json';
+import './style.scss';
 
 const exampleAttributes = {};
 
