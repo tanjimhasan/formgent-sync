3cf9a08fca89c469851ffce4c2e97bcfdc356fc4
mdalaminbey <mdalaminbey@gmail.com>
2024-08-07 10:23:43 +0600

    added some blocks field type

---
+++ app/Fields/Website/MethodResolver.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace FormGent\App\Fields\Website;
+
+defined( 'ABSPATH' ) || exit;
+
+trait MethodResolver {
+    public static function get_key(): string {
+        return 'website';
+    }
+
+    protected function get_validation_rules(): array {
+        return ['string', 'url', 'max:250'];
+    }
+}
