8409ef7cb7840347732834c8e5a34140e2e5247e
tanjim hasan <tanjim.hasan.pro@gmail.com>
2024-08-08 14:24:06 +0600

    updatedd

--- resources/blocks/number/Edit.js
+++ resources/blocks/number/Edit.js
@@ -1,18 +1,25 @@
 import './editor.scss';
+import { RichText } from '@wordpress/block-editor';
 
 export default function Edit( { attributes, setAttributes } ) {
 	return (
 		<div className="formgent-editor-block-list__single">
-			<label
-				className={ `formgent-editor-block-list__single__label formgent-label-align-${ attributes.label_alignment }` }
-			>
-				{ attributes.label }
+			<span>
+				<RichText
+					className={ `formgent-editor-block-list__single__label formgent-label-align-${ attributes.label_alignment }` }
+					tagName="label"
+					value={ attributes.label }
+					onChange={ ( content ) =>
+						setAttributes( { label: content } )
+					}
+					placeholder={ __( 'Type sub label' ) }
+				/>
 				{ attributes.required ? (
 					<span className="formgent-editor-block-list__single__label__required">
 						*
 					</span>
 				) : null }
-			</label>
+			</span>
 			<div className="formgent-editor-block-list__single__wrapper">
 				<input
 					className="formgent-editor-block-list__single__input"
