e3d8557e03f97f840eeb49bb4ab8a1038cd03363
mdalaminbey <mdalaminbey@gmail.com>
2024-08-21 18:07:19 +0600

    using wp_kses_post instead of wp_kses

--- resources/blocks/gdpr/render.php
+++ resources/blocks/gdpr/render.php
@@ -1,10 +1,4 @@
-<?php
-
-defined( 'ABSPATH' ) || exit; 
-
-$allowed_html = wp_kses_allowed_html( 'post' );
-
-?>
+<?php defined( 'ABSPATH' ) || exit; ?>
 
 <div class="formgent-editor-block-list__single formgent-editor-block-list__single--csr">
     <div class="formgent-editor-block-list__single__wrapper formgent-editor-block-list__single__wrapper--gdpr formgent-label-align-<?php echo esc_attr( $attributes['label_alignment'] ); ?>">
@@ -22,7 +16,7 @@ $allowed_html = wp_kses_allowed_html( 'post' );
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#fff" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check"><polyline points="20 6 9 17 4 12"></polyline></svg>
             </span>
             <span class="formgent-editor-block-list__single__sub-label">
-                <?php formgent_render( wp_kses( $attributes['description'], $allowed_html ) ); ?>
+                <?php formgent_render( wp_kses_post( $attributes['description'] ) ); ?>
             </span>
             <?php if ( $attributes['required'] ) : ?>
                 <span class="formgent-editor-block-list__single__label__required">
--- resources/blocks/input-masking/render.php
+++ resources/blocks/input-masking/render.php
@@ -1,17 +1,11 @@
-<?php
-
-defined( 'ABSPATH' ) || exit; 
-
-$allowed_html = wp_kses_allowed_html( 'post' );
-
-?>
+<?php defined( 'ABSPATH' ) || exit; ?>
 
 <div class="formgent-editor-block-list__single formgent-editor-block-list__single--csr">
     <label
         for="<?php echo esc_attr( $attributes['name'] ); ?>"
         class= "formgent-editor-block-list__single__label-container formgent-label-align-<?php echo esc_attr( $attributes['label_alignment'] ); ?>"
     >
-        <?php formgent_render( wp_kses( $attributes['label'], $allowed_html ) ); ?>
+        <?php formgent_render( wp_kses_post( $attributes['label'] ) ); ?>
         <?php if ( $attributes['required'] ) : ?>
             <span class="formgent-editor-block-list__single__label__required">
                 *
@@ -30,7 +24,7 @@ $allowed_html = wp_kses_allowed_html( 'post' );
             data-wp-bind--value="context.data.<?php echo esc_attr( $attributes['name'] ); ?>"
         />
         <span class="formgent-editor-block-list__single__sub-label">
-            <?php formgent_render( wp_kses( $attributes['sub_label'], $allowed_html ) ); ?>
+            <?php formgent_render( wp_kses_post( $attributes['sub_label'] ) ); ?>
         </span>
     </div>
 </div>
