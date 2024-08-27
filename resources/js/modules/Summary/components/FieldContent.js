cf85a9e96247854b948bfba545517c4fb969d540
Shamim Ahmed <shamimeforb@gmail.com>
2024-08-27 08:24:23 +0600

    update

--- resources/js/modules/Summary/components/FieldContent.js
+++ resources/js/modules/Summary/components/FieldContent.js
@@ -1,12 +1,69 @@
-export default function FieldContent( props ) {
-	const { data } = props;
+import ReactSVG from 'react-inlinesvg';
+import envelope from '@icon/envelope.svg';
+import textIcon from '@icon/text.svg';
+import alignLeft from '@icon/align-left.svg';
+import hashIcon from '@icon/hash.svg';
+import linkIcon from '@icon/link.svg';
+import fileIcon from '@icon/file.svg';
 
-	console.log( data );
+export default function FieldContent( props ) {
+	const { summaryFields, summaries, handleOnScroll } = props;
+
+	const fieldIcons = {
+		email: envelope,
+		text: textIcon,
+		textarea: alignLeft,
+		number: hashIcon,
+		website: linkIcon,
+		gdpr: fileIcon,
+	};
 
 	return (
-		data.length &&
-		data.map( ( item ) => {
-			return <span key={ item.id }>{ item.value }</span>;
+		summaryFields.length &&
+		summaryFields.map( ( field, index ) => {
+			const fieldName = field.field_name;
+			return (
+				field.field_type !== 'gdpr' && (
+					<div
+						className={ `formgent-summary-item formgent-summary-item--${ field.field_type }` }
+						key={ index }
+					>
+						<div className="formgent-summary-item__top">
+							<div className="formgent-summary-item__icon">
+								<ReactSVG
+									src={ fieldIcons[ field.field_type ] }
+								/>
+							</div>
+							<div className="formgent-summary-item__title">
+								<h4>{ field.label }</h4>
+								<span>{ `${ field.total_answer } out of ${ field.total_response } people answered this question` }</span>
+							</div>
+						</div>
+						<div
+							className="formgent-summary-item__content"
+							data-field={ field.field_name }
+							onScroll={ ( e ) => {
+								handleOnScroll( e, field.field_name );
+							} }
+						>
+							{ summaries[ fieldName ]?.length > 0 ? (
+								summaries[ fieldName ].map( ( item ) => (
+									<div
+										className="formgent-summary-item__content__single"
+										key={ item.id }
+									>
+										{ item.value }
+									</div>
+								) )
+							) : (
+								<div className="formgent-summary-item__content__not-found">
+									No response!
+								</div>
+							) }
+						</div>
+					</div>
+				)
+			);
 		} )
 	);
 }
--- resources/js/modules/Summary/index.js
+++ resources/js/modules/Summary/index.js
@@ -1,14 +1,8 @@
 import { lazy } from '@wordpress/element';
 import { useSelect, useDispatch, resolveSelect } from '@wordpress/data';
 import { SummaryStyle } from './style';
-import ReactSVG from 'react-inlinesvg';
-import envelope from '@icon/envelope.svg';
-import textIcon from '@icon/text.svg';
-import alignLeft from '@icon/align-left.svg';
-import hashIcon from '@icon/hash.svg';
-import linkIcon from '@icon/link.svg';
-import fileIcon from '@icon/file.svg';
 import { __ } from '@wordpress/i18n';
+import FieldContent from './components/FieldContent';
 const FormHeader = lazy( () => import( '@formgent/components/FormHeader' ) );
 
 function Summary() {
@@ -18,20 +12,7 @@ function Summary() {
 	const { useParams } = CommonReducer?.routerComponents;
 	const { id: formId } = useParams();
 
-	const { FormReducer } = useSelect( ( select ) => {
-		return select( 'formgent' ).getForms();
-	}, [] );
-	const { isFetchingSummary, summary } = FormReducer;
 	const { updateSummaryPerPage } = useDispatch( 'formgent' );
-
-	const fieldIcons = {
-		email: envelope,
-		text: textIcon,
-		textarea: alignLeft,
-		number: hashIcon,
-		website: linkIcon,
-		gdpr: fileIcon,
-	};
 
 	// Retrieve all summary fields for the form ID
 	const summaryFields = useSelect(
@@ -71,62 +52,11 @@ function Summary() {
 			<FormHeader resultHeader />
 			<SummaryStyle>
 				<div className="formgent-summary-content">
-					{ summaryFields.length &&
-						summaryFields.map( ( field, index ) => {
-							const fieldName = field.field_name;
-							return (
-								field.field_type !== 'gdpr' && (
-									<div
-										className={ `formgent-summary-item formgent-summary-item--${ field.field_type }` }
-										key={ index }
-									>
-										<div className="formgent-summary-item__top">
-											<div className="formgent-summary-item__icon">
-												<ReactSVG
-													src={
-														fieldIcons[
-															field.field_type
-														]
-													}
-												/>
-											</div>
-											<div className="formgent-summary-item__title">
-												<h4>{ field.label }</h4>
-												<span>{ `${ field.total_answer } out of ${ field.total_response } people answered this question` }</span>
-											</div>
-										</div>
-										<div
-											className="formgent-summary-item__content"
-											data-field={ field.field_name }
-											onScroll={ ( e ) => {
-												handleInfiniteScroll(
-													e,
-													field.field_name
-												);
-											} }
-										>
-											{ summaries[ fieldName ]?.length >
-											0 ? (
-												summaries[ fieldName ].map(
-													( item ) => (
-														<div
-															className="formgent-summary-item__content__single"
-															key={ item.id }
-														>
-															{ item.value }
-														</div>
-													)
-												)
-											) : (
-												<div className="formgent-summary-item__content__not-found">
-													No response!
-												</div>
-											) }
-										</div>
-									</div>
-								)
-							);
-						} ) }
+					<FieldContent
+						summaryFields={ summaryFields }
+						summaries={ summaries }
+						handleOnScroll={ handleInfiniteScroll }
+					/>
 				</div>
 			</SummaryStyle>
 		</>
