c4797ccaa45fc09a7d413681e71feb5862d8b264
mdalaminbey <mdalaminbey@gmail.com>
2024-09-30 11:48:03 +0600

    Added number decimal support - backend

--- app/Fields/Number/Number.php
+++ app/Fields/Number/Number.php
@@ -8,8 +8,4 @@ use FormGent\App\Fields\Field;
 
 class Number extends Field {
     use MethodResolver;
-
-    protected function get_validation_rules(): array {
-        return ['integer'];
-    }
 }
