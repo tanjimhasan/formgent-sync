76b81e20c830a2155303aa0ed8d587ba42881637
Shamim Ahmed <shamimeforb@gmail.com>
2024-10-01 09:06:50 +0600

    updated

--- resources/blocks/multiple-choice/index.js
+++ resources/blocks/multiple-choice/index.js
@@ -45,6 +45,20 @@ const controls = {
 					},
 				],
 			},
+			style: {
+				type: 'toggle_group',
+				label: __( 'Style', 'formgent' ),
+				options: [
+					{
+						label: __( 'Frame', 'formgent' ),
+						value: 'frame',
+					},
+					{
+						label: __( 'Standard', 'formgent' ),
+						value: 'standard',
+					},
+				],
+			},
 			choice_limit: {
 				type: 'switch',
 				label: __( 'Choice Limit', 'formgent' ),
