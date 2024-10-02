039a937b73a550ed9b921b96b200dad699b355c1
Shamim Ahmed <shamimeforb@gmail.com>
2024-10-02 13:21:37 +0600

    updated blocks tab style

--- resources/blocks/utils.js
+++ resources/blocks/utils.js
@@ -16,9 +16,11 @@ import { registerBlockType } from '@wordpress/blocks';
 import {
 	TabPanel,
 	PanelBody,
-	Dashicon,
 	__experimentalUseSlotFills as useSlotFills,
 } from '@wordpress/components';
+import ReactSVG from 'react-inlinesvg';
+import editIcon from '@icon/edit.svg';
+import cogIcon from '@icon/cog.svg';
 
 const AdvancedControls = () => {
 	const fills = useSlotFills( InspectorAdvancedControls.slotName );
@@ -448,12 +450,20 @@ function Block( { controls, Edit, attributes, setAttributes, metaData } ) {
 							{
 								name: 'general',
 								title: __( 'General', 'formgent' ),
-								icon: <Dashicon icon="admin-generic" />,
+								icon: (
+									<span className="formgent-control-tab-icon">
+										<ReactSVG src={ editIcon } /> General
+									</span>
+								),
 							},
 							{
 								name: 'advanced',
 								title: __( 'Advanced', 'formgent' ),
-								icon: <Dashicon icon="admin-settings" />,
+								icon: (
+									<span className="formgent-control-tab-icon">
+										<ReactSVG src={ cogIcon } /> Advanced
+									</span>
+								),
 							},
 						] }
 					>
